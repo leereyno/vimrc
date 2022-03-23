137cb1fb5876758ae53c7dcf59098047562e55e7
Lee Reynolds <leebreynolds@protonmail.com>
2022-03-22 21:34:24 -0400

    fix typos
    Signed-off-by: Lee Reynolds <leebreynolds@protonmail.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 # Install .vimrc into the home directory of the account specified on the
 # command line
 
@@ -8,13 +7,6 @@ if [ $# -lt 1 ] ; then
 	echo -e "\nGotta give me an account name\n"
 	echo -e "e.g. $(basename $0) myusername\n"
 	exit 1
-=======
-if [ ! -e ~/.vimrc ] ; then
-	cd $(dirname $0)
-	/bin/cp -a .vimrc ~/.vimrc
-else
-	echo -e "\nThe file ~/.vimrc already exists\n"
->>>>>>> ab80aeee23144b05223e16fd4ba0953857eff749
 fi
 
 ACCOUNT=$1
